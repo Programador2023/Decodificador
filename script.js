3cea84a1da66c4b747995cf898fcf4894378e15e
programador2023 <programadorleandro2023@hotmail.com>
2024-02-21 18:59:52 -0300

    posicinei a img, acima dos botões criptografar e descriptografar,retirei o input copiar e acrescentei um botão copia. Inicie o js manipulando elementos na DOM, adicionei a fucionalidade de digitar o texto no textArea, e ao clicar botão ciptrografar, executara a função de esconder minha img principal e exibir o texto digitado

---
+++ script.js
@@ -0,0 +1,18 @@
+// ELEMENTS (onde voce acessa o DOM)
+const wapperAside = document.querySelector(".wapper__aside");
+const encriptarBtn = document.querySelector(".btn__criptografar");
+const textArea = document.querySelector(".input-top");
+
+// FUNCTIONS AND VARIABLES (onde voce manipula o DOM)
+let inputValue;
+function encriptar() {
+wapperAside.innerHTML = inputValue
+}
+
+function changeInput(event) {
+  inputValue = event.target.value;
+}
+
+// EVENT LISTENERS (onde voce observa eventos no DOM )
+encriptarBtn.addEventListener("click", encriptar);
+textArea.addEventListener("input", changeInput);
