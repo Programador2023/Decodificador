919d805e768af55d9a5fd489bf2d18121919ed8e
programador2023 <programadorleandro2023@hotmail.com>
2024-02-26 18:56:20 -0300

    Crei uma variavel textEncriptado, guardei uma string binaria na variavel textEncriptado, posicionei o botão copiar na Dom fora da caixa

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
 // ELEMENTS (onde voce acessa o DOM)
-const wapperAside = document.querySelector(".wapper__aside");
+const wapperAside = document.querySelector(".conteudo-wrapper");
 const encriptarBtn = document.querySelector(".btn__criptografar");
 const textArea = document.querySelector(".input-top");
 
 // FUNCTIONS AND VARIABLES (onde voce manipula o DOM)
 let inputValue;
-function encriptar() {
-wapperAside.innerHTML = inputValue
+let textEncriptado;
 
-let resultCripto = inputValue
+// transforma uma string comum em uma string binaria
+function textoParaBinary(string) {
+  return string
+  .split("")
+  .map(function (char) {
+    return char.charCodeAt(0).toString(2);
+  })
+  .join(" ");
 }
+
+// transforma uma string Binaria em uma string comum
+function binarioParaText(str) {
+  return str
+    .split(" ")
+    .map(function (elem) {
+      return String.fromCharCode(parseInt(elem, 2));
+    })
+    .join("");
+}
+
+function encriptar() {
+  // guardando uma string binaria na var textEncriptado
+  textEncriptado = textoParaBinary(inputValue);
+  wapperAside.innerHTML = textEncriptado
+}
+
+function decriptar() {}
 
 function changeInput(event) {
   inputValue = event.target.value;
 }
 
 // EVENT LISTENERS (onde voce observa eventos no DOM )
-encriptarBtn.addEventListener("click", encriptar);
 textArea.addEventListener("input", changeInput);
+encriptarBtn.addEventListener("click", encriptar);
